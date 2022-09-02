affca6c95e3feb0a7c723b07f17b99495f334f65
Elijah <eligahbunn@gmail.com>
2022-09-02 22:39:04 +0300

    commit1

---
+++ DZ1.cs
@@ -0,0 +1,35 @@
+﻿void FillArray(int[] array, int minimum, int maximum)
+{
+Random random = new Random();
+int i=0;
+while(i<array.Length)
+{
+array[i] = random.Next(minimum,maximum);
+i++;
+}
+}
+
+void PrintArray(int[] array)
+{
+foreach(int element in array)
+Console.Write($"{element} ");
+Console.WriteLine();
+}
+
+int[] array = new int[123];
+FillArray(array,1, 500);
+PrintArray(array);
+
+int numbers1 = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+
+
+if (array[i] >= 10 && array[i] <= 90)
+{
+numbers1++;
+}
+}
+
+Console.WriteLine("В массиве " + numbers1 + " элементов, входящих в указанный диапазон значений" ); 
