c656ae66a5a3efac8db4b5675b1e4b689e6f2cda
Elijahbunn <110155080+Elijahbunn@users.noreply.github.com>
2022-09-02 22:46:12 +0300

    commit 1

---
+++ DZ2.cs
@@ -0,0 +1,29 @@
+﻿void FillArray(int[] array, int minimum, int maximum)
+{
+Random random = new Random();
+int i=0;
+while(i<array.Length)
+{
+array[i] = random.Next(minimum,maximum);
+i++;
+}
+}
+
+void PrintArray(int[] array)
+{
+foreach(int element in array)
+Console.Write($"{element} ");
+Console.WriteLine();
+}
+
+int[] array = new int[14];
+FillArray(array,1, 20);
+PrintArray(array);
+
+int sum = 0;
+
+for (int i=0; i < array.Length / 2; i++)
+{
+sum = array[i] + array[array.Length - i - 1];
+Console.WriteLine ($"{sum} ");
+}
---
+++ DZ3.cs
@@ -0,0 +1,44 @@
+﻿void FillArray(int[] array, int minimum, int maximum)
+{
+Random random = new Random();
+int i=0;
+while(i<array.Length)
+{
+array[i] = random.Next(minimum,maximum);
+i++;
+}
+}
+
+void PrintArray(int[] array)
+{
+foreach(int element in array)
+Console.Write($"{element} ");
+Console.WriteLine();
+}
+
+void PrintNumb(int[] array)
+{
+int i = 0;
+int max = array[i];
+int min = array[i];
+for (i=0; i < array.Length; i++)
+{
+
+if (max < array[i]) max = array[i];
+}
+
+for (i=0; i < array.Length; i++)
+{
+
+if (min > array[i]) min = array[i];
+}
+Console.WriteLine($"Максимальное число массива: {max} ");
+Console.WriteLine($"Минимальное число массива: {min}");
+int numb = max - min;
+Console.WriteLine($"Разница максимума и минимума: {numb}");
+}
+
+int[] array = new int[14];
+FillArray(array,1, 20);
+PrintArray(array);
+PrintNumb(array);
